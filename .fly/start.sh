a2c45d565361baaa85730513312911bef0fc0595
Ryan Quinn <notryanquinn@gmail.com>
2024-05-30 20:54:26 -0700

    switching from vercel to flyio 030

--- .fly/start.sh
+++ .fly/start.sh
@@ -6,4 +6,5 @@ if [ -n $MIGRATE_ON_BOOT ]; then
   $(dirname $0)/migrate.sh
 fi
 
-npx rw-server --port ${PORT} $@
+npx rw-server --api-port 8911 --web-port 8910 --api-host 0.0.0.0 --web-host 0.0.0.0
+
